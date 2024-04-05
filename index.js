c323c3f8e1761db5df7930a739cfb4b9c25a5fc6
Sabaris <sabaris.elango@gmail.com>
2024-04-05 17:19:45 +0530

    Console log user asked weather info via object

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 let place = document.querySelector("#location");
 let search = document.querySelector("#search");
 
+function weatherShowcase(weatherData) {
+  let place = weatherData.location.name;
+  let placeLocation = weatherData.location.country;
+  let temp = weatherData.current.temp_c;
+  let feelsLike = weatherData.current.feelslike_c;
+  let humidity = weatherData.current.humidity;
+  let wind = weatherData.current.wind_kph;
+
+  return { place, placeLocation, temp, feelsLike, humidity, wind };
+}
+
 // async function weather api
 
 async function getWeather() {
   let newLocation = `https://api.weatherapi.com/v1/current.json?key=03afc14dc67c4d7c95d83610241803&q=${place.value}`;
-  console.log(newLocation);
   const response = await fetch(newLocation, { mode: "cors" });
   const weatherData = await response.json();
-  console.log(weatherData);
+  // json to weather object
+  let weatherObject = weatherShowcase(weatherData);
+  return weatherObject;
 }
 
-search.addEventListener("click", getWeather);
+search.addEventListener("click", () => {
+  let weatherInfo = getWeather();
+  console.log(weatherInfo);
+});
